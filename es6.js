bac61321c3a1fed4ca464013572bc6c0e37d55cc
Pavel Khramov <strelprosteam@gmail.com>
2018-08-07 11:40:13 +0300

    add parametrs on default

--- es6.js
+++ es6.js
@@ -39,4 +39,18 @@
 	}
 	obj.logName() 
 }
+Параметры по умолчанию
+{
+	const c = 90;
+
+	const c2 = () => 200;
+
+	const func = (a = 20, b = a + c2()) => {
+
+		return a + b;
+	}
+
+	console.log(func(30))
+}
+
 */
