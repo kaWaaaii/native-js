c46fe9cbda52e26709ffc26049da37b77a733f6f
Pavel Khramov <strelprosteam@gmail.com>
2018-08-08 18:52:30 +0300

    add Generators

--- es6.js
+++ es6.js
@@ -246,4 +246,35 @@ Rest & Spread
 		console.log(n)
 	}
 }
+
+Генераторы
+{
+	function* getRange(start = 0, end = 100, step = 10) {
+		while (start < end) {
+			yield start;
+			start += step;
+		}
+	}
+
+	for (let n of getRange(10, 50)) {
+		console.log(n)
+	}
+
+
+
+	let fib = {
+		*[Symbol.iterator]() {
+			let cur = 1, pre = 0;
+			for (;;) {
+					[cur,pre] = [cur + pre, cur];
+					yield cur;
+			}
+		}
+	}
+
+	for (let n of fib) {
+		if (n>3000) break;
+		console.log(n)
+	}
+}
 */
