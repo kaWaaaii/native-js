f861bb01b99c921f7f9e84a624476d964ced3d8b
Pavel Khramov <strelprosteam@gmail.com>
2018-08-08 18:29:18 +0300

    add symbol

--- es6.js
+++ es6.js
@@ -226,4 +226,24 @@ Rest & Spread
 	console.log(name, age)
 	func();
 }
+
+Символы
+{
+	let fib = {
+		[Symbol.iterator]() {
+			let pre = 0, cur = 1;
+			return {
+				next() {
+					[pre, cur] = [cur, pre + cur];
+					return {value: cur, done: false};
+				}
+			}
+		}
+	}
+
+	for (let n of fib) {
+		if (n > 1500) break;
+		console.log(n)
+	}
+}
 */
